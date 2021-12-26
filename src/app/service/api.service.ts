b1d69e1c1ec201817069399092c59ffd67aaba72
Ezzdin Atef <ezzdin1125@gmail.com>
2021-12-26 13:04:16 +0200

    fix merge

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,12 +1,3 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
-
-  constructor() { }
-=======
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
